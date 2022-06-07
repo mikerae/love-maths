89fe5cb5e74857f56d8fe4da09e60f7bfa11d823
Mike Rae <mikerae@me.com>
2022-06-07 19:34:30 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAddintionQuestion() {
+
+}
+
+function displaySubtractionnQuestion() {
+
+}
+
+function displayMultipliyQuestion() {
+
+}
+
+function displayDivideQuestion() {
+
+}
