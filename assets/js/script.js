c38934465c2b2e0a22e38e8fbbd49b0bb57653df
Mike Rae <mikerae@me.com>
2022-06-07 20:29:59 +0000

    Dispaly addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,19 +10,30 @@ document.addEventListener("DOMContentLoaded", function() {
               alert("You clicked Submit!");
           } else {
               let gameType = this.getAttribute("data-type");
-              alert(`You clicked ${gameType}`);
+              runGame(gameType);
           }
       });
   }
+
+  runGame("addition");
+
 });
 
 /**
  *  The main game "loop", called when the script is first loaded
  *  and after the user's answer has processed
  */
-function runGame() {
+function runGame(gameType) {
+  //Creates two random numbers between 1 and 25
   let num1 = Math.floor(Math.random() * 25) + 1;
   let num2 = Math.floor(Math.random() * 25) + 1;
+
+  if (gameType === "addition") {
+    displayAdditionQuestion(num1, num2);
+  } else {
+    alert(`Unknow game type: ${gameType}`)
+    throw `Unknown game type: ${gameType}. Aborting!`;
+  }
 
 }
 
@@ -42,7 +53,11 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
+
+  document.getElementById('operand1').textContent = operand1;
+  document.getElementById('operand2').textContent = operand2;
+  document.getElementById('operator').textContent = "+";
 
 }
 
