952b31cf7afaa1a489637220ce20c1ba7baa7683
Mike Rae <mikerae@me.com>
2022-06-08 17:55:10 +0000

    Add favicon, and final tidy ups for the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,6 +14,11 @@ document.addEventListener("DOMContentLoaded", function() {
           }
       });
   }
+  document.getElementById("answer-box").addEventListener("keydown", function(event){
+    if (event.key === "Enter") {
+      checkAnswer();
+    }
+  });
 });
 
 /**
@@ -21,6 +26,10 @@ document.addEventListener("DOMContentLoaded", function() {
  *  and after the user's answer has processed
  */
 function runGame(gameType) {
+
+  document.getElementById("answer-box").value = "";
+  document.getElementById("answer-box").focus();
+
   //Creates two random numbers between 1 and 25
   let num1 = Math.floor(Math.random() * 25) + 1;
   let num2 = Math.floor(Math.random() * 25) + 1;
