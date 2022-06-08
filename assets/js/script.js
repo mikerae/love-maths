91225b59eb34fd8f9f1dee5f4ab7327229eeb496
Mike Rae <mikerae@me.com>
2022-06-08 17:25:50 +0000

    Add multpy and subtract code

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,9 +14,6 @@ document.addEventListener("DOMContentLoaded", function() {
           }
       });
   }
-
-  runGame("addition");
-
 });
 
 /**
@@ -30,7 +27,11 @@ function runGame(gameType) {
 
   if (gameType === "addition") {
     displayAdditionQuestion(num1, num2);
-  } else {
+  } else if (gameType === "multiply") {
+    displayMultiplyQuestion(num1, num2);
+  } else if (gameType === "subtract") {
+    displaySubtractQuestion(num1, num2);
+} else {
     alert(`Unknow game type: ${gameType}`)
     throw `Unknown game type: ${gameType}. Aborting!`;
   }
@@ -38,7 +39,7 @@ function runGame(gameType) {
 }
 
 /**
- *  Checks the answe against the first element in
+ *  Checks the answer against the first element in
  * the returned calculateCorrectAnswer array
  */
 function checkAnswer() {
@@ -69,9 +70,13 @@ function calculateCorrectAnswer() {
 
   if (operator === "+") {
     return [operand1 + operand2, "addition"];
+  } else if (operator === "x") {
+    return [operand1 * operand2, "multiply"];
+  } else if (operator === "-") {
+    return [operand1 - operand2, "subtract"];
   } else {
-    alert(`Unimplemeted operator ${operator}`);
-    throw `Unimplemented operator ${operator}. Aorting!`;
+      alert(`Unimplemeted operator ${operator}`);
+      throw `Unimplemented operator ${operator}. Aorting!`;
   }
 }
 
@@ -103,10 +108,20 @@ function displayAdditionQuestion(operand1, operand2) {
 
 }
 
-function displaySubtractQuestion() {
+function displaySubtractQuestion(operand1, operand2) {
+
+  document.getElementById('operand1').textContent = operand1;
+  document.getElementById('operand2').textContent = operand2;
+  document.getElementById('operator').textContent = "-";
 
 }
 
-function displayMultiplyQuestion() {
-  
+function displayMultiplyQuestion(operand1, operand2) {
+
+  document.getElementById('operand1').textContent = operand1;
+  console.log(operand1);
+  console.log(document.getElementById('operand1').textContent);
+  document.getElementById('operand2').textContent = operand2;
+  document.getElementById('operator').textContent = "x";
+
 }
